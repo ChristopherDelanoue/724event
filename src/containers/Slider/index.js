d4a35b45859b2790304d91a1d73d48c6abdf61de
Christopher Delanoue <christopherdelanoue@Mac-Studio.local>
2025-12-16 13:46:49 +0100

    premier commit avec slider ok

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,17 +12,18 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
       5000
     );
   };
   useEffect(() => {
+    if (!byDateDesc || !byDateDesc.length === 0) return;
     nextCard();
   });
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        <div key={event.title}>
           <div
             key={event.title}
             className={`SlideCard SlideCard--${
@@ -40,17 +41,17 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((evt, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={evt.title}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx }
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
